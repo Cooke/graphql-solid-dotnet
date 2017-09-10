68c24e7f83c4996fd8e438a373d9ca1eac47121c
Henrik Cooke <henrik@cooke.se>
2017-09-10 20:16:24 +0200

    Fixed nuget settings and scripts.

---
+++ utils/pack-nuget.bat
@@ -0,0 +1,2 @@
+dotnet pack ../src/Cooke.GraphQL -c RELEASE
+dotnet pack ../src/Cooke.GraphQL.AspNetCore -c RELEASE
---
+++ utils/publish-nuget.bat
@@ -0,0 +1,2 @@
+dotnet nuget push ../src/Cooke.GraphQL/bin/Release/Cooke.GraphQL.%1.nupkg -s https://www.nuget.org/api/v2/package 
+dotnet nuget push ../src/Cooke.GraphQL.AspNetCore/bin/Release/Cooke.GraphQL.AspNetCore.%1.nupkg -s https://www.nuget.org/api/v2/package 
